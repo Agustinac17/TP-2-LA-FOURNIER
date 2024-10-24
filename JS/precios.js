74dd3a226184cf0e857dd40b63684329cc11b9a8
Agus <163650795+Agustinac17@users.noreply.github.com>
2024-10-24 17:32:42 -0300

    cambios nuevos again

--- JS/precios.js
+++ JS/precios.js
@@ -8,28 +8,3 @@ hamburger.addEventListener('click', () => {
 });
 
 ////////////////////////////////////////
-let index = 0;
-
-function mostrarSlide(n) {
-    const slides = document.querySelectorAll('.carrusel-contenedor .producto');
-    if (n >= slides.length) {
-        index = 0;
-    } else if (n < 0) {
-        index = slides.length - 1;
-    } else {
-        index = n;
-    }
-    const offset = -index * (100 / slides.length);
-    document.querySelector('.carrusel-contenedor').style.transform = `translateX(${offset}%)`;
-}
-
-function cambiarSlide(n) {
-    mostrarSlide(index + n);
-}
-
-mostrarSlide(index);
-
-setInterval(() => {
-    cambiarSlide(1);  // Avanza automáticamente cada 3 segundos
-}, 3000);
-
