afdd573d425e657ab45953537f14037cd8ff2c32
Agus <163650795+Agustinac17@users.noreply.github.com>
2024-10-26 07:36:09 -0300

    delete de comentarios

--- funcionesphp/capdatos.php
+++ funcionesphp/capdatos.php
@@ -9,7 +9,6 @@ include '../basededatos/conexion.php';
 
 // Verificar si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recoger los datos del formulario y sanitizarlos
     $event_name = $_POST['event_name'];
     $contact_name = $_POST['contact_name'];
     $contact_number = $_POST['contact_number'];
@@ -21,19 +20,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $con->prepare("INSERT INTO eventos (event_name, contact_name, contact_number, event_date, event_time, event_location) VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("ssssss", $event_name, $contact_name, $contact_number, $event_date, $event_time, $event_location);
 
-    // Ejecutar la consulta y verificar si fue exitosa
     if ($stmt->execute()) {
-        // Redirigir a index.php después de una inserción exitosa
         header("Location: http://localhost/TP-2-LA-FOURNIER/");
         exit();
     } else {
         echo "Error al enviar la solicitud: " . $stmt->error;
     }
 
-    // Cerrar la declaración
     $stmt->close();
 }
 
-// Cerrar la conexión
 $con->close();
 ?>
